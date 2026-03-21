27347c82df82c2ca5be8f0f862d6b508122f9aae
AmirAli <amiralitehrani77@yahoo.com>
2026-03-21 14:10:08 -0400

    Created global.css file and installed all packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
-
-import './App.css'
 
 function App() {
-
+  const options = [
+    { label: 'The Godfather', id: 1 },
+    { label: 'Pulp Fiction', id: 2 },
+  ];
   return (
     <>
-      <section id="center">
-        
+      <section className='center'>
+        <Autocomplete
+          disablePortal
+          options={options}
+          sx={{ width: 300 }}
+          renderInput={(params) => 
+            <TextField {...params} label="Movie" />
+          }
+        />
       </section>
     </>
   )
